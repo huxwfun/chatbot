cea360c78c9c39ccf828a9444fd6268e03190445
胡小为 <huxiaowei@wps.cn>
2023-09-06 17:12:53 +0800

    markdown resolver

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,109 +1,112 @@
 import React from "react";
 import Avatar from "@mui/material/Avatar";
 import { deepOrange } from "@mui/material/colors";
+import ReactMarkdown from 'react-markdown'
+// import remarkGfm from 'remark-gfm'
+
 
 const styles = {
-    messageRow: {
-      display: "flex"
-    },
-    messageRowRight: {
-      display: "flex",
-      justifyContent: "flex-end"
-    },
-    messageBlue: {
-      position: "relative",
-      marginLeft: "20px",
-      marginBottom: "10px",
-      padding: "10px",
-      backgroundColor: "#A8DDFD",
-      width: "60%",
-      //height: "50px",
-      textAlign: "left",
-      font: "400 .9em 'Open Sans', sans-serif",
-      border: "1px solid #97C6E3",
-      borderRadius: "10px",
-      "&:after": {
-        content: "''",
-        position: "absolute",
-        width: "0",
-        height: "0",
-        borderTop: "15px solid #A8DDFD",
-        borderLeft: "15px solid transparent",
-        borderRight: "15px solid transparent",
-        top: "0",
-        left: "-15px"
-      },
-      "&:before": {
-        content: "''",
-        position: "absolute",
-        width: "0",
-        height: "0",
-        borderTop: "17px solid #97C6E3",
-        borderLeft: "16px solid transparent",
-        borderRight: "16px solid transparent",
-        top: "-1px",
-        left: "-17px"
-      }
-    },
-    messageOrange: {
-      position: "relative",
-      marginRight: "20px",
-      marginBottom: "10px",
-      padding: "10px",
-      backgroundColor: "#f8e896",
-      width: "60%",
-      //height: "50px",
-      textAlign: "left",
-      font: "400 .9em 'Open Sans', sans-serif",
-      border: "1px solid #dfd087",
-      borderRadius: "10px",
-      "&:after": {
-        content: "''",
-        position: "absolute",
-        width: "0",
-        height: "0",
-        borderTop: "15px solid #f8e896",
-        borderLeft: "15px solid transparent",
-        borderRight: "15px solid transparent",
-        top: "0",
-        right: "-15px"
-      },
-      "&:before": {
-        content: "''",
-        position: "absolute",
-        width: "0",
-        height: "0",
-        borderTop: "17px solid #dfd087",
-        borderLeft: "16px solid transparent",
-        borderRight: "16px solid transparent",
-        top: "-1px",
-        right: "-17px"
-      }
-    },
-
-    messageContent: {
-      padding: 0,
-      margin: 0
-    },
-    messageTimeStampRight: {
+  messageRow: {
+    display: "flex"
+  },
+  messageRowRight: {
+    display: "flex",
+    justifyContent: "flex-end"
+  },
+  messageBlue: {
+    position: "relative",
+    marginLeft: "20px",
+    marginBottom: "10px",
+    padding: "10px",
+    backgroundColor: "#A8DDFD",
+    width: "60%",
+    //height: "50px",
+    textAlign: "left",
+    font: "400 .9em 'Open Sans', sans-serif",
+    border: "1px solid #97C6E3",
+    borderRadius: "10px",
+    "&:after": {
+      content: "''",
       position: "absolute",
-      fontSize: ".85em",
-      fontWeight: "300",
-      marginTop: "10px",
-      bottom: "-3px",
-      right: "5px"
+      width: "0",
+      height: "0",
+      borderTop: "15px solid #A8DDFD",
+      borderLeft: "15px solid transparent",
+      borderRight: "15px solid transparent",
+      top: "0",
+      left: "-15px"
     },
-
-    orange: {
-      backgroundColor: deepOrange[500],
-    },
-    avatarNothing: {
-      color: "transparent",
-      backgroundColor: "transparent",
-    },
-    displayName: {
-      marginLeft: "20px"
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      width: "0",
+      height: "0",
+      borderTop: "17px solid #97C6E3",
+      borderLeft: "16px solid transparent",
+      borderRight: "16px solid transparent",
+      top: "-1px",
+      left: "-17px"
     }
+  },
+  messageOrange: {
+    position: "relative",
+    marginRight: "20px",
+    marginBottom: "10px",
+    padding: "10px",
+    backgroundColor: "#f8e896",
+    width: "60%",
+    //height: "50px",
+    textAlign: "left",
+    font: "400 .9em 'Open Sans', sans-serif",
+    border: "1px solid #dfd087",
+    borderRadius: "10px",
+    "&:after": {
+      content: "''",
+      position: "absolute",
+      width: "0",
+      height: "0",
+      borderTop: "15px solid #f8e896",
+      borderLeft: "15px solid transparent",
+      borderRight: "15px solid transparent",
+      top: "0",
+      right: "-15px"
+    },
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      width: "0",
+      height: "0",
+      borderTop: "17px solid #dfd087",
+      borderLeft: "16px solid transparent",
+      borderRight: "16px solid transparent",
+      top: "-1px",
+      right: "-17px"
+    }
+  },
+
+  messageContent: {
+    padding: 0,
+    margin: 0
+  },
+  messageTimeStampRight: {
+    position: "absolute",
+    fontSize: ".85em",
+    fontWeight: "300",
+    marginTop: "10px",
+    bottom: "-3px",
+    right: "5px"
+  },
+
+  orange: {
+    backgroundColor: deepOrange[500],
+  },
+  avatarNothing: {
+    color: "transparent",
+    backgroundColor: "transparent",
+  },
+  displayName: {
+    marginLeft: "20px"
+  }
 }
 
 export const MessageLeft = (props: any) => {
@@ -122,9 +125,7 @@ export const MessageLeft = (props: any) => {
         <div>
           <div style={styles.displayName}>{displayName}</div>
           <div style={styles.messageBlue as any}>
-            <div>
-              <p style={styles.messageContent}>{message}</p>
-            </div>
+            <ReactMarkdown>{message}</ReactMarkdown>
             <div style={styles.messageTimeStampRight as any}>{timestamp}</div>
           </div>
         </div>
@@ -138,8 +139,9 @@ export const MessageRight = (props: any) => {
   return (
     <div style={styles.messageRowRight}>
       <div style={styles.messageOrange as any}>
-        <p style={styles.messageContent}>{message}</p>
-        <div style={styles.messageTimeStampRight as any}>{timestamp}</div>
+        <ReactMarkdown>{message}</ReactMarkdown>
+        {/* <p style={styles.messageContent}>{message}</p> */}
+        {/* <div style={styles.messageTimeStampRight as any}>{timestamp}</div> */}
       </div>
     </div>
   );
